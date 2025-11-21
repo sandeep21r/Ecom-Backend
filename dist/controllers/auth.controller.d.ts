87006c7fff811f6ca90fbde8038ce29940e9fd85
sandeep.s <sandeep.s@dpworld.com>
2025-11-22 04:25:49 +0530

    login api

--- dist/controllers/auth.controller.d.ts
+++
@@ -1,4 +0,0 @@
-import { Request, Response } from "express";
-export declare const googleLogin: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-export declare const otpLogin: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-//# sourceMappingURL=auth.controller.d.ts.map
--- dist/utils/generateJwt.d.ts
+++
@@ -1,3 +0,0 @@
-import { Types } from "mongoose";
-export declare const generateToken: (userId: string | Types.ObjectId) => string;
-//# sourceMappingURL=generateJwt.d.ts.map
